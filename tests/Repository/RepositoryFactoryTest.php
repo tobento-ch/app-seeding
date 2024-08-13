e4490acb07e5247e27dfbd34c177a829d72ba248
TOBENTOch <info@tobento.ch>
2024-08-13 15:36:18 +0200

    fixed test

--- tests/Repository/RepositoryFactoryTest.php
+++ tests/Repository/RepositoryFactoryTest.php
@@ -14,17 +14,49 @@ declare(strict_types=1);
 namespace Tobento\App\Seeding\Test\Repository;
 
 use PHPUnit\Framework\TestCase;
+use Tobento\App\AppInterface;
+use Tobento\App\AppFactory;
 use Tobento\App\Seeding\FactoryInterface;
 use Tobento\App\Seeding\Repository\RepositoryFactory;
 use Tobento\App\Seeding\Repository\AbstractFactory;
+use Tobento\Service\Filesystem\Dir;
 use Tobento\Service\Repository\Storage\StorageRepository;
 use Tobento\Service\Repository\Storage\Column;
 use Tobento\Service\Storage\InMemoryStorage;
 
 class RepositoryFactoryTest extends TestCase
 {
+    protected function createApp(bool $deleteDir = true): AppInterface
+    {
+        if ($deleteDir) {
+            (new Dir())->delete(__DIR__.'/../app/');
+        }
+        
+        (new Dir())->create(__DIR__.'/../app/');
+        
+        $app = (new AppFactory())->createApp();
+        
+        $app->dirs()
+            ->dir(realpath(__DIR__.'/../../'), 'root')
+            ->dir(realpath(__DIR__.'/../app/'), 'app')
+            ->dir($app->dir('app').'config', 'config', group: 'config')
+            ->dir($app->dir('root').'vendor', 'vendor');
+        
+        $app->boot(\Tobento\App\Seeding\Boot\Seeding::class);
+        $app->booting();
+        
+        return $app;
+    }
+    
+    public static function tearDownAfterClass(): void
+    {
+        (new Dir())->delete(__DIR__.'/../app/');
+    }
+    
     public function testNewMethodReturnsFactory()
     {
+        $app = $this->createApp();
+        
         $repo = new class(
             storage: new  InMemoryStorage(items: []),
             table: 'users',
@@ -34,7 +66,7 @@ class RepositoryFactoryTest extends TestCase
             ],
         ) extends StorageRepository {
             //
-        };        
+        };
 
         $factory = RepositoryFactory::new(repository: $repo);
         
